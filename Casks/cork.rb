a832c69848310c257d7c0afb241e9c4ad89a8e09
David Bureš <buresdv@gmail.com>
2024-06-30 00:43:35 +0200

    + Cork cask

---
+++ Casks/cork.rb
@@ -0,0 +1,14 @@
+cask "cork" do
+    version "1.4.3"
+    sha256 :no_check
+
+    url "https://corkmac.app/RLS/#{version}/Cork.zip"
+    name "Cork"
+    desc "The Ultimate Homebrew Companion"
+    homepage "https://www.corkmac.app"
+
+    auto_updates false
+    depends_on macos: ">= :ventura"
+
+    app "Cork.app"
+end
