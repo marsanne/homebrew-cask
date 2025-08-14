3fb4e4e13d3986a9ae3a480d0e4e68bded07bc3c
David Bureš <buresdv@gmail.com>
2025-08-14 15:02:36 +0200

    More Cork experimentation

--- Casks/cork-versioned-debug.rb
+++ Casks/cork-versioned-debug.rb
@@ -1,25 +1,25 @@
 cask "cork-versioned-debug" do
+    sha256 :no_check
+
     on_ventura do
         version "1.5.6"
-        sha256 :no_check
 
         url "https://corkmac.app/RLS/macos-13/Cork.zip"
         
     end
 
-    on sonoma :or_newer do
+    on_sonoma :or_newer do
         version "1.5.6"
-        sha256 :no_check
 
         url "https://corkmac.app/RLS/#{version}/Cork.zip"
     end
 
     name "Cork"
-        desc "The Ultimate Homebrew Companion"
-        homepage "https://www.corkmac.app"
+    desc "The Ultimate Homebrew Companion"
+    homepage "https://www.corkmac.app"
 
-        auto_updates false
-        depends_on macos: ">= :ventura"
+    auto_updates false
+    depends_on macos: ">= :ventura"
 
-        app "Cork.app"
+    app "Cork.app"
 end
